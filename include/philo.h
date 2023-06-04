6f637e5c0641bea804ed38bfba3b5ee0345591ef
Casper Schuijt <casperschuijt@gmail.com>
2023-06-04 13:53:26 +0200

    First version philo struct

--- include/philo.h
+++ include/philo.h
@@ -10,4 +10,21 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#ifndef PHILO_H
+# define PHILO_H
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <stdbool.h>
+
+typedef struct s_philosopher {
+	int				id;
+	pthread_t		*thread;
+	pthread_mutex_t	*fork_l;
+	pthread_mutex_t	*fork_r;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				time_to_think;
+}	t_philosopher;
+
+#endif
