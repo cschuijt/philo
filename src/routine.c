ca5704807dd3f963f663e2ec61165a7f54eb7182
Casper Schuijt <casperschuijt@gmail.com>
2023-06-04 23:23:46 +0200

    Start routine

---
+++ src/routine.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   routine.c                                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: cschuijt <cschuijt@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/06/04 21:54:30 by cschuijt      #+#    #+#                 */
+/*   Updated: 2023/06/04 21:54:30 by cschuijt      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	*philosopher_routine(void *philo_struct)
+{
+	t_philosopher	*philo;
+
+	philo = (t_philosopher *) philo_struct;
+}
