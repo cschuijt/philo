b5e7d5109fe07cb5a84bd7fbcdf9e7f50bbff494
Casper Schuijt <cschuijt@student.codam.nl>
2023-06-22 14:07:01 +0200

    Restructure routine one last time

--- philo/src/routine.c
+++ philo/src/routine.c
@@ -20,33 +20,8 @@ static void	update_philo_eat_stats(t_philosopher *philo)
 	pthread_mutex_unlock(&philo->eat_stats_mutex);
 }
 
-static void	run_routine_with_delay(t_philosopher *philo)
-{
-	pthread_mutex_lock(&(philo->state->state_mutex));
-	pthread_mutex_unlock(&(philo->state->state_mutex));
-	scuffed_sleep(1);
-	while (get_status(philo->state))
-	{
-		print_with_time(philo, "is thinking");
-		pthread_mutex_lock(philo->fork_l);
-		print_with_time(philo, "has taken a fork");
-		pthread_mutex_lock(&(philo->fork_r));
-		print_with_time(philo, "has taken a fork");
-		update_philo_eat_stats(philo);
-		print_with_time(philo, "is eating");
-		scuffed_sleep(philo->state->time_to_eat);
-		pthread_mutex_unlock(philo->fork_l);
-		pthread_mutex_unlock(&(philo->fork_r));
-		print_with_time(philo, "is sleeping");
-		scuffed_sleep(philo->state->time_to_sleep);
-	}
-	return ;
-}
-
 static void	run_routine(t_philosopher *philo)
 {
-	pthread_mutex_lock(&(philo->state->state_mutex));
-	pthread_mutex_unlock(&(philo->state->state_mutex));
 	while (get_status(philo->state))
 	{
 		print_with_time(philo, "is thinking");
@@ -70,10 +45,11 @@ void	*philosopher_routine(void *philo_struct)
 	t_philosopher	*philo;
 
 	philo = (t_philosopher *) philo_struct;
-	if (philo->id % 2)
-		run_routine(philo);
-	else
-		run_routine_with_delay(philo);
+	pthread_mutex_lock(&(philo->state->state_mutex));
+	pthread_mutex_unlock(&(philo->state->state_mutex));
+	if (!(philo->id % 2))
+		scuffed_sleep(1);
+	run_routine(philo);
 	print_with_time(philo, "has died");
 	shut_down_simulation(philo->state);
 	return (NULL);
